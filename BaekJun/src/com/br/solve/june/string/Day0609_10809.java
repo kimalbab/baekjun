fc5fbbf48db43dbdb06433a6cb06ef5ad7899198
kimalbab <kimalbab@gmail.com>
2022-10-10 15:26:40 +0900

    update : 10809 buffered reader 수정
    scanner 사용시 248ms 속도가 148ms로 줄어들음

--- BaekJun/src/com/br/solve/june/string/Day0609_10809.java
+++ BaekJun/src/com/br/solve/june/string/Day0609_10809.java
@@ -1,10 +1,12 @@
 package com.br.solve.june.string;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Day0609_10809 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//일단 첫번째 반복의 경우는 알파벳 갯수만큼? 검색해야한다..
 		//예를 들어 첫글자인 b가 알파벳 어디에 있는지 알파벳마다 돌면서 검색해야함
 		//만약 b가 있다면 출력한다 : b가 몇번째 알파벳인지....
@@ -14,8 +16,11 @@ public class Day0609_10809 {
 		//count를 해 준다..?, 그게 아니면 count = -1
 		
 		//먼저 알파벳 배열.. 소문자 알파벳 배열을 문자화시켜서 배열을 만들어야한다.
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
+		//Scanner sc = new Scanner(System.in);
+		//String s = sc.nextLine();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
 		String[] sArr = new String[s.length()];
 		String[] arr = new String[26];
 		
